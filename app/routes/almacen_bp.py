1130710af117f5e16e252bad804e02fad831ec1e
gerz <gerz970@gmail.com>
2025-04-03 01:50:35 -0600

    fix

--- app/routes/almacen_bp.py
+++ app/routes/almacen_bp.py
@@ -5,3 +5,11 @@ from utils.decorators import role_required
 from core.logic import compras
 
 almacen_bp = Blueprint('almacen_bp', __name__)
+
+@almacen_bp.route("/almacen/compras_list", methods=['GET'])
+def get_compras():
+    """
+    Obtiene el listado completo de compras activas, incluyendo sus detalles.
+    """
+    return jsonify(compras.list_all())
+
